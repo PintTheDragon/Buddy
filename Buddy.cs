6c33749a028b50a7943740b41fa90445b23668b1
PintTheDragon <PintTheDragon@hotmail.com>
2020-06-22 18:00:55 -0700

    Updated for Exiled 2.0.0

---
+++ Buddy.cs
@@ -0,0 +1,70 @@
+﻿using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+
+namespace Buddy
+{
+    class BuddyPluginEXILED : Plugin<Config>
+    {
+        public EventHandlersEXILED EventHandlers;
+
+        public string BuddyMessage = "Hey! If you would like to play with a friend, type $buddyCMD <friend's name>.";
+
+        public  string BuddyMessagePrompt = "Hey! %name wants to play with you. Type %buddyAcceptCMD to accept!";
+
+        public string buddyCommand = "buddy";
+
+        public string buddyAcceptCommand = "baccept";
+
+        public string roundAlreadyStartedMessage = "This command can only be ran before the round starts.";
+
+        public string alreadyHaveBuddyMessage = "You already have a buddy.";
+
+        public string playerNotFoundMessage = "The player was not found.";
+
+        public string buddyRequestSentMessage = "Request sent!";
+
+        public string noBuddyRequestsMessage = "You do not have any buddy requests.";
+
+        public string errorMessage = "An error occured.";
+
+        public string buddyRequestAcceptMessage = "Your buddy request was accepted!";
+
+        public string successMessage = "Success!";
+
+        public Dictionary<string, ReferenceHub> buddies = new Dictionary<string, ReferenceHub>();
+
+        public Dictionary<string, ReferenceHub> buddyRequests = new Dictionary<string, ReferenceHub>();
+
+        public string prefixedMessage = "";
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted -= EventHandlers.OnRoundStart;
+            Exiled.Events.Handlers.Player.Joined -= EventHandlers.OnPlayerJoin;
+            Exiled.Events.Handlers.Server.SendingConsoleCommand -= EventHandlers.OnConsoleCommand;
+            Log.Info("Buddy (by PintTheDragon) has unloaded.");
+        }
+
+        public override void OnEnabled()
+        {
+            
+            if (!Config.IsEnabled)
+            {
+                this.OnDisabled();
+                Log.Info("Disregard any further messages about the plugin being enabled. It has been disabled.");
+                return;
+            }
+            this.prefixedMessage = this.BuddyMessage.Replace("$buddyCMD", "." + buddyCommand);
+            EventHandlers = new EventHandlersEXILED(this);
+            Exiled.Events.Handlers.Server.RoundStarted += EventHandlers.OnRoundStart;
+            Exiled.Events.Handlers.Player.Joined += EventHandlers.OnPlayerJoin;
+            Exiled.Events.Handlers.Server.SendingConsoleCommand += EventHandlers.OnConsoleCommand;
+        }
+
+        public override void OnReloaded()
+        {
+            //Really nothing to do here for me
+        }
+    }
+}
---
+++ Config.cs
@@ -0,0 +1,28 @@
+﻿using Exiled.API.Interfaces;
+using Exiled.Loader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buddy
+{
+    class Config : IConfig
+    {
+        public bool IsEnabled { get; set; }
+
+        public Boolean forceExactRole { get; set; }
+
+        public Boolean disallowGuardScientistCombo { get; set; }
+
+        public string Prefix => "buddy_";
+
+        public void Reload()
+        {
+            IsEnabled = PluginManager.YamlConfig.GetBool($"{Prefix}enabled", true);
+            forceExactRole = PluginManager.YamlConfig.GetBool($"{Prefix}force_exact_role", false);
+            forceExactRole = PluginManager.YamlConfig.GetBool($"{Prefix}disallow_guard_scientist_combo", true);
+        }
+    }
+}
